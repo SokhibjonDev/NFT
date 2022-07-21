d1d528ab6b71e069818980b4e64ac4a3a7b05e48
SokhibjonDev <105203720+SokhibjonDev@users.noreply.github.com>
2022-07-21 13:25:35 +0500

    feat : react-js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <ul>
+            <li>
+              <a href="#!">Salom</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
